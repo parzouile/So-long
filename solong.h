1fd965587a0b2305c28bca38f41a6cb3b841ef92
aschmitt <alban.schmitt@hotmail.com>
2024-01-13 23:07:13 +0100

    petit bou par petit bou

--- solong.h
+++ solong.h
@@ -6,26 +6,40 @@
 /*   By: aschmitt <aschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 20:18:05 by aschmitt          #+#    #+#             */
-/*   Updated: 2024/01/12 12:56:31 by aschmitt         ###   ########.fr       */
+/*   Updated: 2024/01/13 23:02:13 by aschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SOLONG_H
 # define SOLONG_H
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
 
 typedef struct	s_player {
 	int     x;
 	int     y;
 	int		size;
+	void	*img;
 }				t_player;
+
+typedef struct	s_coins {
+	int     x;
+	int     y;
+	int		alive;
+	void	*img;
+}				t_coins;
+
+typedef struct s_data
+{
+	void		*mlx_ptr;
+	void		*win_ptr;
+	void		*map;
+	t_player	player;
+	t_coins		coins;
+	int			width;
+	int			height;
+	int			n;
+}				t_data;
+
 
 typedef struct	s_vars {
 	void	*mlx;
--- test.c
+++ test.c
@@ -6,23 +6,17 @@
 /*   By: aschmitt <aschmitt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 13:58:41 by aschmitt          #+#    #+#             */
-/*   Updated: 2024/01/12 17:19:55 by aschmitt         ###   ########.fr       */
+/*   Updated: 2024/01/13 23:05:59 by aschmitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ml/mlx.h"
+#include "solong.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <X11/X.h>
 #include <X11/keysym.h>
  
-typedef struct s_data
-{
-	void *mlx_ptr;
-	void *win_ptr;
-	int x;
-	int y;
-} t_data;
+
  
 int on_destroy(t_data *data)
 {
@@ -32,57 +26,114 @@ int on_destroy(t_data *data)
 	exit(0);
 	return (0);
 }
+void	print_map(t_data *data)
+{
+	int	x;
+	int	y;
+	void	*map;
+	int		width;
+	int		height;
+
+	map = mlx_xpm_file_to_image(data->mlx_ptr, "asset/Gras.xpm", &width, &height);
+	x = -1;
+	while (++x < 13)
+	{
+		printf("x = %d\n", x);
+		y = -1;
+		while (++y < 5)
+		{
+			printf("y = %d\n", y);
+			mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, map, x * 49, y * 49);
+		}
+	}
+}
+
+void	print_xpm(t_data *data)
+{
+	print_map(data);
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->coins.img, data->coins.x, data->coins.y);
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->player.img, data->player.x, data->player.y);
+	
+}
+
+void	set_pos(t_data *data, int x, int y)
+{
+	if (x + data->player.size > data->width)
+		x = data->width - data->player.size;
+	else if (x < 0)
+		x = 0;
+	if (y + data->player.size > data->height)
+		y = data->height - data->player.size;
+	else if (y < 0)
+		y = 0;
+	if (data->player.x != x || data->player.y != y)
+	{
+		data->player.x = x;
+		data->player.y = y;
+		data->n++;
+		printf("count: %d\n", data->n);
+	}
+}
+
+int	check_pos(t_data *data)
+{
+	(void)data;
+	return(1);
+}
  
 int on_keypress(int keysym, t_data *data)
 {
-	int width;
-	int height;
-
-	printf("Pressed key: %d\n", keysym);
+	
 	if	(keysym == 65307)
 		on_destroy(data);
 	if (keysym == 100)
-	{
-		void *map;
-		void *perso;
-		map = mlx_xpm_file_to_image(data->mlx_ptr, "asset/map.xpm", &width, &height);
-		perso = mlx_xpm_file_to_image(data->mlx_ptr, "asset/homer_carre.xpm", &width, &height);
-		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, map, 0, 0);
-		data->x = data->x + 100;
-		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, perso, data->x , data->y);
-	}
+		set_pos(data, data->player.x + data->player.size, data->player.y);
+	if (keysym == 113)
+		set_pos(data, data->player.x - data->player.size, data->player.y);
+	if (keysym == 115)
+		set_pos(data, data->player.x, data->player.y + data->player.size);
+	if (keysym == 122)
+		set_pos(data, data->player.x, data->player.y - data->player.size);
+	if (check_pos(data))
+		print_xpm(data);
+	else
+		on_destroy(data);
 	return (0);
 }
+
+
  
 int main(void)
 {
 	t_data data;
-	void	*img;
-	void	*perso;
 	int		width;
 	int		height;
 
 	data.mlx_ptr = mlx_init();
 	if (!data.mlx_ptr)
 		return (1);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, 1280, 720, "hi");
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 13 * 49, 5 * 49, "hi");
 	if (!data.win_ptr)
-		return (free(data.mlx_ptr), 1);	
-	 
+		return (free(data.mlx_ptr), 1);
+	
+	data.height = 5 * 49;
+	data.width = 13 * 49;
+	data.n = 0;
 	// Register key release hook
 	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
-	mlx_mouse_hook(data.win_ptr, &on_keypress, &data);	
  
 	// Register destroy hook
 	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
-	
-	img = mlx_xpm_file_to_image(data.mlx_ptr, "asset/homer_carre.xpm", &width, &height);
-	perso = mlx_xpm_file_to_image(data.mlx_ptr, "asset/map.xpm", &width, &height);
-	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, perso, 0, 0);
-	mlx_put_image_to_window(data.mlx_ptr, data.win_ptr, img, 250, 250);
-	data.x = 250;
-	data.y = 250;
-	
+
+	data.map = mlx_xpm_file_to_image(data.mlx_ptr, "asset/map.xpm", &width, &height);
+	data.player.x = 0;
+	data.player.y = 0;
+	data.player.size = 49;
+	data.player.img = mlx_xpm_file_to_image(data.mlx_ptr, "asset/homer_49.xpm", &width, &height);
+	data.coins.img = mlx_xpm_file_to_image(data.mlx_ptr, "asset/donut.xpm", &width, &height);
+	data.coins.x = 5 * 49;
+	data.coins.y = 2 * 49;
+	print_xpm(&data);
 	mlx_loop(data.mlx_ptr);
 	return (0);
 }
